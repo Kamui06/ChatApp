d870f9ec3367cb09a3fe5e11336386e68c19f15c
Kamui06 <harshpatel132109@gmail.com>
2026-06-23 18:59:21 +0530

    git add index.js

--- server/index.js
+++ server/index.js
@@ -106,6 +106,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    server.listen(5000, () => console.log("Server running on http://localhost:5000"));
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.error("DB connection error:", err));
